bd741b316449a7a9b2c525d9a00a1294ff64d8a3
MahreenKhan12 <mahreenkhan@Mahreens-MacBook-Air.local>
2016-03-12 15:27:54 -0500

    log in attempt

--- functions.php
+++ functions.php
@@ -150,3 +150,5 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+add_filter('wp_nav_menu_items', 'add_login_logout_link', 10, 2); function add_login_logout_link($items, $args) {         ob_start();         wp_loginout('index.php');         $loginoutlink = ob_get_contents();         ob_end_clean();         $items .= '<li>'. $loginoutlink .'</li>';     return $items; }
