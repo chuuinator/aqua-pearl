1ad8e42e61037b885080eb0eeb8c278d380ac1a2
chuuinator <Jessica@Jessicas-MacBook-Air.local>
2016-03-14 22:26:30 -0400

    update-footer

--- footer.php
+++ footer.php
@@ -14,14 +14,15 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+			<div id="footer-menu">
+			<?php wp_nav_menu(array('theme_location'=>'secondary', 'menu_class'=>'foot-menu'));?>
+		</div><!--footer-menu-->
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'aqua-pearl' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'aqua-pearl' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'aqua-pearl' ), 'aqua-pearl', '<a href="http://phoenix.sheridanc.on.ca/~ccit3472" rel="designer">TMJ</a>' ); ?>
 		</div><!-- .site-info -->
-		<div id="footer-menu">
-			<?php wp_nav_menu(array('theme_location'=>'secondary', 'menu_class'=>'foot-menu'));?>
-		</div><!--footer-menu-->
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
